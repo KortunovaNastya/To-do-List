596d795a88bb420062d3e221546d5e7cc4e32fe4
KortunovaNastya <143782792+KortunovaNastya@users.noreply.github.com>
2024-04-02 11:50:50 +0300

    Add files via upload

---
+++ To-Do-List2-master/To-Do-List2/AllZadachi.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace To_Do_List2
+{
+    /// <summary>
+    /// Логика взаимодействия для AllZadachi.xaml
+    /// </summary>
+    public partial class AllZadachi : Window
+    {
+        public AllZadachi()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Account account = new Account();
+            account.Show();
+            this.Close();
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ To-Do-List2-master/To-Do-List2/NewZadacha.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace To_Do_List2
+{
+    /// <summary>
+    /// Логика взаимодействия для NewZadacha.xaml
+    /// </summary>
+    public partial class NewZadacha : Window
+    {
+        public NewZadacha()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();   
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Account account = new Account();
+            account.Show();
+            this.Close();
+        }
+    }
+}
